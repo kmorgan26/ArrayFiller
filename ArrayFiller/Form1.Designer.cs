d53ec87c6d8ea1d1ab4f888a7617075eceabddb1
Kevin Morgan <kmorgan26@gmail.com>
2020-06-23 14:59:42 -0500

    Add project files.

---
+++ ArrayFiller/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace ArrayFiller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtIterations = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GO!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(88, 154);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(60, 20);
+            this.txtMin.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Numbers to fill";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "between";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(154, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "and";
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(185, 154);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(60, 20);
+            this.txtMax.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "How Many Iterations?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(547, 42);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Unique Number Range Selector";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(30, 61);
+            this.label7.MaximumSize = new System.Drawing.Size(1000, 500);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(732, 49);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "This is a tool that will test how many iterations it will take to select, at rand" +
+    "om, each number in a range before all numbers have been selected at least once";
+            // 
+            // txtIterations
+            // 
+            this.txtIterations.Location = new System.Drawing.Point(157, 197);
+            this.txtIterations.Name = "txtIterations";
+            this.txtIterations.Size = new System.Drawing.Size(60, 20);
+            this.txtIterations.TabIndex = 11;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(369, 145);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(318, 134);
+            this.listBox1.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(366, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Results";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(366, 293);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(82, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Rolling Average";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(373, 317);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(36, 39);
+            this.lblAverage.TabIndex = 15;
+            this.lblAverage.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(611, 286);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "CLEAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(770, 379);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtIterations);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Number Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtIterations;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ArrayFiller/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ArrayFiller
+{
+    public partial class Form1 : Form
+    {
+        static Random random = new Random();
+        public int range;
+        public int min;
+        public int max;
+        public int totalIterations = 0;
+        public int totalCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Main();
+        }
+
+        //Returns a random number between the min and max
+        private int GetRandomNumber()
+        {
+            try
+            {
+                return random.Next(min, max);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private void Main()
+        {
+            try
+            {
+                //=======Set initial values from Textboxes============
+                int iterations = Convert.ToInt32(txtIterations.Text);
+                min = Convert.ToInt32(txtMin.Text);
+                max = Convert.ToInt32(txtMax.Text) + 1;
+
+                //===The Range is how many numbers it will have to "fill"=========
+                range = max - min;
+
+                //------ for/next loop that will fill the array as many times as iterations 
+                for (int x = 0; x < iterations; x++)
+                {
+                    //Keeps track of total iterations. Reset only on Reset button click
+                    totalIterations++;
+
+                    //Create a new list to contain the numbers
+                    List<int> myList = new List<int>();
+
+                    //Start the counter at 1 at each "GO!" button press
+                    int counter = 1;
+
+                    //Do Loop will continue until the List<int> is filled
+                    do
+                    {
+                        //Get random number
+                        int valueToAdd = GetRandomNumber();
+
+                        //if the List doesn't contain the number...
+                        if (!myList.Contains(valueToAdd))
+                        {
+                            //add the number to the list, increase the counter and total count
+                            myList.Add(valueToAdd);
+                            counter++;
+                            totalCount++;
+                        }
+                        else
+                        {
+                            //Do not add, but increase both counters
+                            counter++;
+                            totalCount++;
+                        }
+                    } while (myList.Count < this.range);
+
+                    //create the string to display in the ListBox for each Iteration
+                    string lines = String.Format("{0} attempts to get {1} unique numbers", counter - 1, myList.Count);
+
+                    //Add the summary string to the listbox
+                    listBox1.Items.Add(lines);
+
+                    //Display the running average
+                    lblAverage.Text = (totalCount / totalIterations).ToString();
+                }
+            }
+            catch
+            {
+                //Just a generic error handler that catches any errors
+                string error = "You did something wrong";
+                listBox1.Items.Add(error);
+            }
+        }
+
+        //=======Clear Button==================================
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            lblAverage.Text = "0";
+            totalIterations = 0;
+            totalCount = 0;
+        }
+    }
+}
